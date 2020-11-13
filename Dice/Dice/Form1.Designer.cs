978d8410654078e178bec2135ae3601122b0577a
Anna <anna.h.hallberg.92@gmail.com>
2020-11-13 20:03:36 +0100

    Create Random function

--- Dice/Dice/Form1.Designer.cs
+++ Dice/Dice/Form1.Designer.cs
@@ -29,13 +29,88 @@ namespace Dice
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.DiceNumber1 = new System.Windows.Forms.Label();
+            this.DiceNumber3 = new System.Windows.Forms.Label();
+            this.DiceNumber2 = new System.Windows.Forms.Label();
+            this.DiceSum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(150, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hit your dices";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DiceNumber1
+            // 
+            this.DiceNumber1.AutoSize = true;
+            this.DiceNumber1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiceNumber1.Location = new System.Drawing.Point(150, 103);
+            this.DiceNumber1.Name = "DiceNumber1";
+            this.DiceNumber1.Size = new System.Drawing.Size(26, 29);
+            this.DiceNumber1.TabIndex = 1;
+            this.DiceNumber1.Text = "1";
+            // 
+            // DiceNumber3
+            // 
+            this.DiceNumber3.AutoSize = true;
+            this.DiceNumber3.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiceNumber3.Location = new System.Drawing.Point(321, 103);
+            this.DiceNumber3.Name = "DiceNumber3";
+            this.DiceNumber3.Size = new System.Drawing.Size(26, 29);
+            this.DiceNumber3.TabIndex = 2;
+            this.DiceNumber3.Text = "1";
+            // 
+            // DiceNumber2
+            // 
+            this.DiceNumber2.AutoSize = true;
+            this.DiceNumber2.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiceNumber2.Location = new System.Drawing.Point(235, 103);
+            this.DiceNumber2.Name = "DiceNumber2";
+            this.DiceNumber2.Size = new System.Drawing.Size(26, 29);
+            this.DiceNumber2.TabIndex = 3;
+            this.DiceNumber2.Text = "1";
+            // 
+            // DiceSum
+            // 
+            this.DiceSum.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiceSum.Location = new System.Drawing.Point(12, 177);
+            this.DiceSum.Name = "DiceSum";
+            this.DiceSum.Size = new System.Drawing.Size(477, 29);
+            this.DiceSum.TabIndex = 4;
+            this.DiceSum.Text = "Sum";
+            this.DiceSum.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(517, 403);
+            this.Controls.Add(this.DiceSum);
+            this.Controls.Add(this.DiceNumber2);
+            this.Controls.Add(this.DiceNumber3);
+            this.Controls.Add(this.DiceNumber1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Dices";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label DiceNumber1;
+        private System.Windows.Forms.Label DiceNumber3;
+        private System.Windows.Forms.Label DiceNumber2;
+        private System.Windows.Forms.Label DiceSum;
     }
 }
 
--- Dice/Dice/Form1.cs
+++ Dice/Dice/Form1.cs
@@ -16,5 +16,21 @@ namespace Dice
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var randNum = new Random();
+            var dice = new int[3];
+
+            for (int rollingDice = 0; rollingDice < 3; rollingDice++)
+            {
+                dice[rollingDice] = randNum.Next(1, 7);
+            }
+
+            DiceNumber1.Text = dice[0].ToString();
+            DiceNumber2.Text = dice[1].ToString();
+            DiceNumber3.Text = dice[2].ToString();
+
+        }
     }
 }
